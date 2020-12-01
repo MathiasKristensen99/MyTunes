c810e2e9bb38ff9d4b18b3f7087db526e55ca867
MathiasKristensen99 <mathiaskristensen52@gmail.com>
2020-12-01 11:27:26 +0100

    Playlist

--- src/com/company/be/Playlist.java
+++ src/com/company/be/Playlist.java
@@ -1,4 +1,12 @@
 package com.company.be;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Playlist {
+    private List<Song> playlist;
+
+    public Playlist() {
+        playlist = new ArrayList<>();
+    }
 }

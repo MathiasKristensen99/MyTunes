4b46bab0d6a698c5e88a6b10c78ff22fc1646407
Zacharias Nielsen <zach0066@gmail.com>
2020-12-02 12:04:16 +0100

    iuh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //Julian er fucking scheisse
-        // jens har kæmpe pik
+        //jens har kæmpe pik
     }
 }

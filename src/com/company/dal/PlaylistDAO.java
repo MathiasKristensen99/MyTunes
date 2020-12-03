c1d03caa41eaea341cb72dc85aefbb480b1b73b4
MathiasKrarupMadsen <Math249e@easv365.dk>
2020-12-03 11:34:07 +0100

    commit

--- src/com/company/dal/PlaylistDAO.java
+++ src/com/company/dal/PlaylistDAO.java
@@ -1,4 +1,28 @@
 package com.company.dal;
 
+import com.company.be.Playlist;
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlaylistDAO {
+
+    PlaylistSongsDAO PlaylistSongInfo = new PlaylistSongsDAO();
+    SQLServerDataSource ds;
+}
+
+public PlaylistDAO() throws IOException {
+    this.ds = new SQLServerDataSource();
+    DatabaseConnectionDAO connectionInfo = new DatabaseConnectionDAO();
+    List<String> infoList = connectionInfo.getDatabaseInfo();
+    ds.setDatabaseName(infoList.get(0));
+    ds.setUser(infoList.get(1));
+    ds.setPassword(infoList.get(2));
+    ds.setPortNumber(Integer.parseInt(infoList.get(3)));
+    ds.setServerName(infoList.get(4));
+}
+    public List<Playlist> getPlaylists() {
+        List<Playlist> Playlists = new ArrayList<>();
 }
